b6a05765fd7a465c0f711a58761bf023aa7dfbed
Adekunle Adeyemi <adekunleadeyemi@Adekunles-MacBook-Pro.local>
2024-03-02 02:20:36 +0000

    updating data.tf

--- data.tf
+++ data.tf
@@ -46,12 +46,3 @@ data "aws_iam_policy_document" "generic_endpoint_policy" {
     }
   }
 }
-
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
-    bucket = "eu-west-2-terraformstate"
-    key    = "dev/terraform.tfstate"
-    region = "eu-west-2"
-  }
-}
